c6b2b9500180c0f7d1461b67b269f4cd3a889053
Usama Shahid <usamashahid.us8@gmail.com>
2023-12-18 20:27:53 +0000

    chore: type updates

--- src/inference.py
+++ src/inference.py
@@ -6,14 +6,14 @@ from langchain.llms import Ollama
 from langchain.prompts import PromptTemplate
 
 
-def load_transform(docs_path):
+def load_transform(docs_path: str):
     doc_chunks = load(docs_path)
     vector_store = transform(doc_chunks)
     retriever = vector_store.as_retriever()
     return retriever
 
 
-def setup(docs_path):
+def setup(docs_path: str):
     llm = Ollama(model='stable-beluga', temperature=0)
     prompt = PromptTemplate.from_template(PROMPT_TEMPLATE)
     retriever = load_transform(docs_path)
